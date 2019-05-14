78509d1d9b9938fcd3f2bbc2dbbce529a88969dd
git-walter <15221024927@163.com>
2019-05-15 00:35:27 +0800

    spring AOP-xml

--- spring/src/main/java/com/walter/service/MyAspect.java
+++ spring/src/main/java/com/walter/service/MyAspect.java
@@ -1,33 +1,31 @@
 package com.walter.service;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
-@Aspect
+
 @Component
 public class MyAspect {
 
-    @Before("execution(* *..StudentServiceImpl.addStudent())")
+
     public void before() {
         System.out.println("前置通知");
     }
 
-    @After("execution(* *..StudentServiceImpl.selectStudent())")
+
     public void after() {
         System.out.println("最终通知");
     }
 
-    @AfterThrowing(value = "execution(* *..StudentServiceImpl.selectStudentById(..))",throwing="e")
+
     public void afterThrowing(Exception e) {
         System.out.println("异常通知");
     }
-    @AfterReturning(value = "execution(* *..StudentServiceImpl.updateStudent())",returning =
-            "result")
+
     public void afterReturing(int result) {
         System.out.println("后置通知");
     }
-    @Around("execution(* *..StudentServiceImpl.deleteStudent())")
+
     public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("环绕通知：前");
         Object proceed = proceedingJoinPoint.proceed();
--- spring/src/main/java/com/walter/service/StudentServiceImpl.java
+++ spring/src/main/java/com/walter/service/StudentServiceImpl.java
@@ -1,16 +1,9 @@
 package com.walter.service;
 
-import com.walter.bean.Student;
-import com.walter.dao.StudentDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service("studentServiceImpl")
 public class StudentServiceImpl implements StudentService {
-    @Autowired
-    @Qualifier("studentDaoImpl")
-    private StudentDao studentDao;
 
 
     @Override
